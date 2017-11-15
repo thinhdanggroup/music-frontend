65344f5666a3fa23de666d81b56dbab635546f60
Vu Tran <tinyboy186@gmail.com>
2017-11-15 15:03:14 +0700

    merge

--- src/routing-path.js
+++ src/routing-path.js
@@ -1,7 +1,4 @@
 export const MAIN_PAGE = '/'
 export const BAI_HAT = '/bai-hat/:idBaiHat'
-<<<<<<< HEAD
 export const CA_NHAN = '/ca-nhan/'
-=======
 export const ALBUM = '/album/:idAlbum'
->>>>>>> d9fe9a824d030e7c944489641e6d149b55d280c8
