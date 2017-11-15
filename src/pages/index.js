3fd9ad5ca62ace2c398afd43c9a946820d49cd78
khanhkiem <buidoigiauco@gmail.com>
2017-11-15 18:27:08 +0700

    mergeing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,4 @@
 export { default as Main } from './main'
 export { default as BaiHat } from './bai-hat'
-<<<<<<< HEAD
 export { default as CaNhan } from './ca-nhan'
-=======
 export { default as Album } from './album'
->>>>>>> d9fe9a824d030e7c944489641e6d149b55d280c8
