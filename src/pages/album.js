265942c76ac2042fe85065d59a449380ed857299
thinhdanggroup <dinhluchvc@gamil.com>
2017-11-15 23:57:37 +0700

    read a part of json getAlbumByID

--- src/pages/album.js
+++ src/pages/album.js
@@ -6,27 +6,47 @@ class Album extends Component {
     super(props);
 
     this.state = {
-      albumID: null
+      moTa: null,
+      namPhatHanh: null,
+      ten: null,
+      baiHats: null,
+      ngheSiPHs: null
     };
-
+    
     this.afterGetAlbum = this.afterGetAlbum.bind(this)
   }
 
-  componentWillMount() {
+  componentDidMount() {
     redux.actions.getAlbumById(this.props.match.params.idAlbum, this.afterGetAlbum)
+  }
+  componentWillMount() {
+    // redux.actions.getAlbumById(this.props.match.params.idAlbum, this.afterGetAlbum)
   }
 
   render() {
-    console.log(this.state.albumID)
+    console.log(this.state.baiHats);
+    // const listItem = this.state.baiHats.map((value,index) =>
+    //  (
+    //    <li key={index}> {value.ID} </li>
+    //  )
+    // );
     return (
       <div>
-        ALBUM
+        <p> Ten Album : {this.state.ten} </p>
+        <p> Thong tin : {this.state.moTa} </p>
+    
       </div>
     )
   }
-
   afterGetAlbum(albumID) {
-    this.setState({ albumID: albumID })
+    this.setState(
+      { 
+        moTa: albumID.data.data.getalbumbyid.MO_TA,
+        namPhatHanh: albumID.data.data.getalbumbyid.NAM_PHAT_HANH,
+        ten: albumID.data.data.getalbumbyid.TEN,
+        baiHats: albumID.data.data.getalbumbyid.baiHats,
+        ngheSiPHs: albumID.data.data.getalbumbyid.ngheSiPHs
+      })
   }
 }
 
