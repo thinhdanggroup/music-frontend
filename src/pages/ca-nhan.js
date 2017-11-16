6e61c80cb7606b3ceb546ac04c2ad3ab3b6c01e3
Vu Tran <tinyboy186@gmail.com>
2017-11-16 10:39:38 +0700

    Add code to show data for Personal Page

--- src/pages/ca-nhan.js
+++ src/pages/ca-nhan.js
@@ -7,7 +7,10 @@ class CaNhan extends Component {
 
     this.state = {
       email: 'garen@gmail.com',
-      info: null
+      hoTen: null,
+      taiKhoan: null,
+      playlists: null,
+      rates: null
     }
 
     this.afterGetInfo = this.afterGetInfo.bind(this)
@@ -18,16 +21,47 @@ class CaNhan extends Component {
   }
 
   render() {
-    console.log(this.state.info)
+    var playlistItems = <li> N/A </li>
+    if (this.state.playlists != null) {
+      playlistItems = this.state.playlists.map((value, index) =>
+        <li key={index}>
+          Ten: {value.TEN} -
+          Thoi diem tao: {value.THOI_DIEM_TAO} -
+          Thoi diem cap nhat: {value.THOI_DIEM_CAP_NHAT}
+        </li>)
+    }
+
+    var rateItems = <li> N/A </li>
+    if (this.state.rates != null) {
+      rateItems = this.state.rates.map((value, index) =>
+        <li key={index}>
+          Ten: {value.TEN} -
+          So sao: {value.SO_SAO}
+        </li>)
+    }
+
     return (
       <div>
-        PERSONAL PAGE
+        <p> Personal Page </p>
+        <p> Ho Ten: {this.state.hoTen} </p>
+        <p> Tai khoan: {this.state.taiKhoan} </p>
+        <p> Playlists: </p>
+        <p> {playlistItems} </p>
+        <p> Ratings: </p>
+        <p> {rateItems} </p>
       </div>
     )
   }
 
   afterGetInfo(info) {
-    this.setState({ info: info })
+    console.log(info)
+    const infoData = info.data.data.getinfoforpersonalpage
+    this.setState({
+      hoTen: `${infoData.HO} ${infoData.TEN_LOT} ${infoData.TEN}`,
+      taiKhoan: infoData.TAI_KHOAN,
+      playlists: infoData.memPlaylists,
+      rates: infoData.memRates
+    })
   }
 }
 
