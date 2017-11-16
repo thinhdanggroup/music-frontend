73b5fbd54be100fa25b55907c1beb4bc0c1f9f86
khanhkiem <buidoigiauco@gmail.com>
2017-11-16 18:29:26 +0700

    edit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import './styles/css/main.css';
+import './styles/scss/main.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
