a3f1e701bc90128c03d83cea27f9103b26d138bf
khanhkiem <buidoigiauco@gmail.com>
2017-11-16 11:37:06 +0700

    porting

--- src/containers/RootContainer.jsx
+++ src/containers/RootContainer.jsx
@@ -3,11 +3,11 @@ import { connect } from 'react-redux';
 
 import { initEnvironment } from '../actions/EnvironmentActions';
 import { initRouter } from '../actions/RouterActions';
-import { initAuth } from '../actions/SessionActions';
+// import { initAuth } from '../actions/SessionActions';
 import Root from '../components/Root';
 import SongContainer from '../containers/SongContainer';
-import SongsContainer from '../containers/SongsContainer';
-import UserContainer from '../containers/UserContainer';
+// import SongsContainer from '../containers/SongsContainer';
+// import UserContainer from '../containers/UserContainer';
 
 import {
   INDEX_PATH,
@@ -26,16 +26,16 @@ const mapStateToProps = (state) => {
     paths: [INDEX_PATH, PLAYLIST_PATH, SONG_PATH, SONGS_PATH, USER_PATH],
     router,
     routes: {
-      [INDEX_PATH]: SongsContainer,
-      [PLAYLIST_PATH]: SongsContainer,
+      // [INDEX_PATH]: SongsContainer,
+      // [PLAYLIST_PATH]: SongsContainer,
       [SONG_PATH]: SongContainer,
-      [SONGS_PATH]: SongsContainer,
-      [USER_PATH]: UserContainer,
+      // [SONGS_PATH]: SongsContainer,
+      // [USER_PATH]: UserContainer,
     },
   };
 };
 
-
+const initAuth = () => { }
 export default connect(mapStateToProps, {
   initAuth,
   initEnvironment,
