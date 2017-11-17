44691fec064e8068fed63e936ecd8e1bc88714fb
Vu Tran <tinyboy186@gmail.com>
2017-11-17 16:06:30 +0700

    Add code to select email

--- src/pages/bai-hat.js
+++ src/pages/bai-hat.js
@@ -6,30 +6,62 @@ class BaiHat extends Component {
     super(props);
 
     this.state = {
-      baiHat: null
+      ten: null,
+      memsEmail: null,
+      selectedEmail: 'N/A',
+      memPlaylistsName: null,
+      selectedPlaylist: null
     }
 
     this.afterGetBaiHat = this.afterGetBaiHat.bind(this)
+    this.afterGetMembersEmail = this.afterGetMembersEmail.bind(this)
+    this.handleEmailChange = this.handleEmailChange.bind(this)
   }
 
   componentWillMount() {
     const { idBaiHat } = this.props.match.params
-
     redux.actions.getBaiHatById(idBaiHat, this.afterGetBaiHat)
+    redux.actions.getMembersEmail(this.afterGetMembersEmail)
   }
 
   render() {
-    console.log(this.state.baiHat)
+    var emailItems = ''
+    if (this.state.memsEmail != null) {
+      emailItems = this.state.memsEmail.map(generateEmailItem)
+    }
+
     return (
       <div>
-        BAI HAT PAGE
-      </div>
+        <p> BAI HAT PAGE </p>
+        <p> Ten: {this.state.ten} </p>
+        <p> Email: </p>
+        <select onChange={this.handleEmailChange}>
+          {emailItems}
+        </select>
+      </div >
     )
   }
 
-  afterGetBaiHat(baiHat) {
-    this.setState({ baiHat: baiHat })
+  afterGetBaiHat(info) {
+    console.log(info)
+    const infoData = info.data.data.getbaihatbyid
+    this.setState({ ten: infoData.title })
   }
+
+  afterGetMembersEmail(info) {
+    console.log(info)
+    const infoData = info.data.data.getmembersemail
+    this.setState({ memsEmail: infoData.membersEmail })
+  }
+
+  handleEmailChange(event) {
+    this.setState({ selectedEmail: event.target.value })
+  }
+}
+
+function generateEmailItem(emailObject) {
+  var emailItems = <option key={emailObject.EMAIL} value={emailObject.EMAIL}> {emailObject.EMAIL} </option>
+  return emailItems
 }
 
 export default BaiHat
