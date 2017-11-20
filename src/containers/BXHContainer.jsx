c65c6710100d868d4439e83a406231922dd1f87b
thinhdanggroup <dinhluchvc@gamil.com>
2017-11-20 11:12:04 +0700

    merge

--- src/containers/BXHContainer.jsx
+++ src/containers/BXHContainer.jsx
@@ -1,16 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-<<<<<<< HEAD
-import Album from '../components/Album';
-// import { playSong } from '../actions/PlayerActions';
-import { navigateTo } from '../actions/RouterActions';
-// import { login, toggleFollow, toggleLike } from '../actions/SessionActions';
-import fetchAlbumIfNeeded from '../actions/AlbumActions';
-import { getId, getIsAuthenticated, getLikes, getPlayingSongId, getSidebarHeight } from '../selectors/CommonSelectors';
-import { getPlaylist, getShouldFetchUser, getSongs, getAlbum } from '../selectors/AlbumSelectors';
-
-const BXHContainer = props => <Album {...props} />;
-=======
 import BXH from '../components/BXH';
 // import { playSong } from '../actions/PlayerActions';
 import { navigateTo } from '../actions/RouterActions';
@@ -20,17 +9,12 @@ import { getId, getIsAuthenticated, getLikes, getPlayingSongId, getSidebarHeight
 import { getPlaylist, getShouldFetchUser, getSongs, getAlbum } from '../selectors/AlbumSelectors';
 
 const BXHContainer = props => <BXH {...props} />;
->>>>>>> 03616af847cc54582e70babab35ccc2716161ffe
 
 const mapStateToProps = (state) => {
   const { player } = state;
 
   return {
-<<<<<<< HEAD
-    id: '0000001',
-=======
     id: getId(state),
->>>>>>> 03616af847cc54582e70babab35ccc2716161ffe
     isAuthenticated: getIsAuthenticated(state),
     likes: getLikes(state),
     player,
@@ -49,11 +33,7 @@ const toggleFollow = () => { }
 const toggleLike = () => { }
 
 export default connect(mapStateToProps, {
-<<<<<<< HEAD
-  fetchAlbumIfNeeded,
-=======
   fetchBXHIfNeeded,
->>>>>>> 03616af847cc54582e70babab35ccc2716161ffe
   login,
   toggleFollow,
   toggleLike,
