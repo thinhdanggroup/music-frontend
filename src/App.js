21f405b19ef3f72c2a4b18e5a45cd38b696a0c63
Vu Tran <tinyboy186@gmail.com>
2017-11-24 19:17:02 +0700

    Fix bug

--- src/App.js
+++ src/App.js
@@ -7,35 +7,8 @@ import configureStore from './store/configureStore';
 export default class App extends Component {
   render() {
     return (
-      <Provider store={redux.store}>
-        <BrowserRouter>
-          <Switch>
-            <Route
-              exact={true}
-              path={routingPath.MAIN_PAGE}
-              component={pages.Main} />
-            <Route
-              exact={true}
-              path={routingPath.BAI_HAT}
-              component={pages.BaiHat} />
-            <Route
-              exact={true}
-              path={routingPath.CA_NHAN}
-              component={pages.CaNhan} />
-            <Route
-              exact={true}
-              path={routingPath.PLAYLIST}
-              component={pages.Playlist} />
-            <Route
-              exact={true}
-              path={routingPath.ALBUM}
-              component={pages.Album} />
-            <Route
-              exact={true}
-              path={routingPath.ARTIST}
-              component={pages.Artist} />
-          </Switch>
-        </BrowserRouter>
+      <Provider store={configureStore()}>
+        <RootContainer />
       </Provider>
     )
   }
